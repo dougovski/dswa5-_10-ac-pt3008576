b0c96aeff5ec39daaa099c9dd1f20c1427a046ab
dougovski <88936801+dougovski@users.noreply.github.com>
2022-04-14 20:59:14 -0300

    Update consulta.js

--- consulta.js
+++ consulta.js
@@ -1,7 +1,20 @@
-const mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb+srv://dswa5:dswa5@cluster0.bpnnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
-                    {useUnifiedTopology: true}),
-                    (error, connection) => {
-                        if(error) return console.log(error);
-                        global.connection = connection.db("ifsp");
-});
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb+srv://dswa5:dswa5@cluster0.bpnnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+
+const dbName = 'ifsp';
+
+async function main() {
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(ifsp);
+  const collection = db.collection('contatos');
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
