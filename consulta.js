afa085478a141e96ea9090da38e48c1b65e2b89f
dougovski <88936801+dougovski@users.noreply.github.com>
2022-04-14 20:49:19 -0300

    Update consulta.js

--- consulta.js
+++ consulta.js
@@ -1,1 +1,7 @@
-
+const mongoClient = require("mongodb").MongoClient;
+mongoClient.connect("mongodb+srv://dswa5:dswa5@cluster0.bpnnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+                    {useUnifiedTopology: true}),
+                    (error, connection) => {
+                        if(error) return console.log(error);
+                        global.connection = connection.db("ifsp");
+});
